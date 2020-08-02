6585781633ed80a409997403533b1580fc3bb3f2
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 11:38:36 -0700

    Update 10-get-the-source.sh

--- packages-to-add/10-get-the-source.sh
+++ packages-to-add/10-get-the-source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd rpmbuild/SOURCES
-wget https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/python-ioprocess-0.15.1-1.el7.noarch.rpm
+
 # Get the source to build the packages with
 sudo cp 11-packages.repo /etc/yum.repos.d/
 reposync -n --source --repo ovirt-4.4-src  --newest-only
