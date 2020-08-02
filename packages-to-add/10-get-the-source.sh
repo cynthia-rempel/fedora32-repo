55bf4005b527d816019c721e76ed1fc8c92e81d8
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 08:54:40 -0700

    Update 10-get-the-source.sh
    Save 10G of download

--- packages-to-add/10-get-the-source.sh
+++ packages-to-add/10-get-the-source.sh
@@ -2,4 +2,4 @@
 # Get the source to build the packages with
 mkdir SRPMS
 cd SRPMS
-wget -r -A .rpm https://resources.ovirt.org/pub/ovirt-4.4/rpm/el8/SRPMS/
+wget -r -A .rpm https://resources.ovirt.org/pub/ovirt-4.4/rpm/el8/SRPMS/ --reject appliance
