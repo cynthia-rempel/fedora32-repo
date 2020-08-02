e4c5e05fb561e084b5365ea196fb3c957ea9c8da
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 10:29:52 -0700

    Update 20-patch-the-source.sh

--- packages-to-add/20-patch-the-source.sh
+++ packages-to-add/20-patch-the-source.sh
@@ -4,9 +4,11 @@
 sudo yum-builddep ./*.rpm > log.txt 2>&1
 grep matching log.txt | sort | uniq | awk '{print $6}'
 
+
 # ovirt-engine-nodejs-modules
 sed 's/Source0.*/Source0\:\ yarn-offline-cache.tar/' -i ../SPECS/ovirt-engine-nodejs-modules.spec
 sed 's/\%..yarn./yarn-1.22.0.js/' -i ../SPECS/ovirt-engine-nodejs-modules.spec
-
+# build the patched .spec
+rpmbuild -bb ../SPECS/ovirt-engine-nodejs-modules.spec
 # finally, test installing the patched RPM.
 sudo dnf install ../RPMS/noarch/ovirt-engine-nodejs-modules-2.0.30-1.fc32.noarch.rpm
