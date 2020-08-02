2d6d6e412fbb4c407e23759483c78b445088db7e
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 08:37:51 -0700

    Create 00-create-repo.sh
    Add a skeleton of some ideas that should be in the repo

---
+++ 00-create-repo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# configure fedora32 to trust fedora32 packages
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-32-x86_64
+
+# install packages
+sudo dnf install httpd createrepo yum-utils
+
+# create the repo directory
+sudo mkdir -p /var/repo
+
+# finally once the packages are added, run create repo
+
+# set the service to start on reboot and now
+sudo systemctl enable httpd
+sudo systemctl start httpd
+# Using repo locally so not allowing the traffic through
+# firewall-cmd --zone=public --permanent --add-service=http
+# firewall-cmd --zone=public --permanent --add-service=https
+# firewall-cmd --reload
+
+# install the .repo file
+
+# test the repo by installing packages
+
+# References:
+#   https://www.redhat.com/sysadmin/apache-yum-dnf-repo
