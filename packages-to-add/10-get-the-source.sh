47748285c3b2efc5937900f142e0ffebb2090663
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 09:27:00 -0700

    Update 10-get-the-source.sh
    Only download newest versions of packages

--- packages-to-add/10-get-the-source.sh
+++ packages-to-add/10-get-the-source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get the source to build the packages with
-sudo dnf localinstall ovirt-release44.rpm
-mkdir SRPMS
-cd SRPMS
-wget -r -A .rpm https://resources.ovirt.org/pub/ovirt-4.4/rpm/el8/SRPMS/ --reject appliance
+sudo cp 11-packages.repo /etc/yum.repos.d/
+reposync -n --source --repo ovirt-4.4-src  --newest-only
+
+# TODO: exclude appliance and NodeJS packages
