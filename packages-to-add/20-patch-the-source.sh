4ee9be2639a23b94c83d28510f5feabd42c3fb6c
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 10:36:40 -0700

    Update 20-patch-the-source.sh

--- packages-to-add/20-patch-the-source.sh
+++ packages-to-add/20-patch-the-source.sh
@@ -6,6 +6,8 @@ grep matching log.txt | sort | uniq | awk '{print $6}'
 
 
 # ovirt-engine-nodejs-modules
+sudo yum-builddep ovirt-engine-nodejs-modules-2.0.30-1.el8.src.rpm
+rpmbuild --rebuild ovirt-engine-nodejs-modules-2.0.30-1.el8.src.rpm
 sed 's/Source0.*/Source0\:\ yarn-offline-cache.tar/' -i ../SPECS/ovirt-engine-nodejs-modules.spec
 sed 's/\%..yarn./yarn-1.22.0.js/' -i ../SPECS/ovirt-engine-nodejs-modules.spec
 # build the patched .spec
