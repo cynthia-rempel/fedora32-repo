19063509787d0dbbfd0869ef8acfc82428d8945d
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 09:39:23 -0700

    Update 20-patch-the-source.sh

--- packages-to-add/20-patch-the-source.sh
+++ packages-to-add/20-patch-the-source.sh
@@ -1,1 +1,5 @@
 # Placeholder to acknowledge the patching step
+
+# identify which packages need to be installed for a build to succeed
+sudo yum-builddep ./*.rpm > log.txt 2>&1
+grep matching log.txt | sort | uniq | awk '{print $6}'
