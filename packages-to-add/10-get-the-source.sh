3361dd6ebfca8761844155d5c02dcbd039ba4efa
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 09:09:40 -0700

    Update 10-get-the-source.sh

--- packages-to-add/10-get-the-source.sh
+++ packages-to-add/10-get-the-source.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Get the source to build the packages with
+sudo dnf localinstall ovirt-release44.rpm
 mkdir SRPMS
 cd SRPMS
 wget -r -A .rpm https://resources.ovirt.org/pub/ovirt-4.4/rpm/el8/SRPMS/ --reject appliance
