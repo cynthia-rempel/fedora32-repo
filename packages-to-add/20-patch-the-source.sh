eb72cc36f9c852475eb9c9b312cc8ea17ed86bf0
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-02 13:46:32 -0700

    Update 20-patch-the-source.sh

--- packages-to-add/20-patch-the-source.sh
+++ packages-to-add/20-patch-the-source.sh
@@ -123,6 +123,10 @@ sudo dnf install ../RPMS/noarch/ovirt-engine-extension-aaa-jdbc-1.2.0-1.fc32.noa
 sudo yum-builddep ovirt-engine-extension-aaa-ldap-1.4.0-1.el8.src.rpm 
 rpmbuild --rebuild ovirt-engine-extension-aaa-ldap-1.4.0-1.el8.src.rpm 
 sudo dnf install ../RPMS/noarch/ovirt-engine-extension-aaa-ldap-*
+
+sudo yum-builddep ovirt-engine-extension-aaa-misc-1.1.0-1.el8.src.rpm
+rpmbuild --rebuild ovirt-engine-extension-aaa-misc-1.1.0-1.el8.src.rpm 
+sudo dnf install ../RPMS/noarch/ovirt-engine-extension-aaa-misc-1.1.0-1.fc32.noarch.rpm
   
 # sudo yum-builddep ovirt-engine-api-explorer-0.0.6-1.el8.src.rpm
 # missing ovirt-engine-yarn
