16909474f153d64fa9984c9ba7d85cb2af454c66
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-07 12:54:51 -0700

    Update 01-setup-build-environment.sh

--- 01-setup-build-environment.sh
+++ 01-setup-build-environment.sh
@@ -54,6 +54,9 @@ bash -x mv-noarch.sh
 mock -r fedora-32-x86_64 clean
 mock -r fedora-32-x86_64 init
 
+mock -r fedora-32-x86_64 engine-db-query-1.6.1-1.fc32.src.rpm
+find /var/lib/mock/fedora-32-x86_64/root/ | grep noarch'\.'rpm$ | awk '{print "mv "$1" /var/repo/noarch"}' > mv-noarch.sh
+
 sudo mkdir -p /var/repo/{noarch,x86_64}
 sudo mv /var/lib/mock/fedora-32-x86_64/result/*.noarch.rpm /var/repo/noarch/
 
