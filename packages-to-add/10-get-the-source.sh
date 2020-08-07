6f32fb2c87fd2c10db7511389254f896e545bfde
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-07 10:08:04 -0700

    Update 10-get-the-source.sh

--- packages-to-add/10-get-the-source.sh
+++ packages-to-add/10-get-the-source.sh
@@ -4,5 +4,5 @@ cd rpmbuild/SOURCES
 # Get the source to build the packages with
 sudo cp 11-packages.repo /etc/yum.repos.d/
 reposync -n --source --repo ovirt-4.4-src  --newest-only
-wget http://vault.centos.org/7.8.2003/os/Source/SPackages/cracklib-2.9.0-11.el7.src.rpm
-# TODO: exclude appliance and NodeJS packages
+# wget http://vault.centos.org/7.8.2003/os/Source/SPackages/cracklib-2.9.0-11.el7.src.rpm
+# TODO: exclude appliance package
