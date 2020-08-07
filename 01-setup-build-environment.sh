509f315f97ccb13ea971a4b48a5cd9a195104752
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-07 10:02:09 -0700

    Update 01-setup-build-environment.sh

--- 01-setup-build-environment.sh
+++ 01-setup-build-environment.sh
@@ -5,5 +5,14 @@ rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-32-x86_64
 dnf -y install mock
 usermod -a -G mock cindy
 
+# add the source repo
+tee -a /etc/yum.repos.d/ovirt-src.repo << END
+[ovirt-4.4-src]
+name=Latest oVirt 4.4 Release
+baseurl=https://resources.ovirt.org/pub/ovirt-4.4/rpm/el8/SRPMS/
+enabled=0
+gpgcheck=0
+END
+
 # Reference:
 #   https://blog.packagecloud.io/eng/2015/05/11/building-rpm-packages-with-mock/
