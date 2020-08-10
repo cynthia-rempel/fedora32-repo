f44bf826d3c9045fb856c704f54f079f735df1ff
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-08-09 19:59:26 -0700

    Update 01-setup-build-environment.sh

--- 01-setup-build-environment.sh
+++ 01-setup-build-environment.sh
@@ -187,6 +187,7 @@ mock -r fedora-32-x86_64 --chain ovirt-engine-extensions-api-1.0.1-1.fc32.src.rp
 mock -r fedora-32-x86_64 ovirt-engine-extensions-api-1.0.1-1.fc32.src.rpm
 find /var/lib/mock/fedora-32-x86_64/root/ | grep noarch'\.'rpm$ | awk '{print "mv "$1" /var/repo/noarch"}' > mv-noarch.sh
   567  sudo bash -x mv-noarch.sh
+mock -r fedora-32-x86_64 ovirt-engine-nodejs-modules-2.0.30-1.fc32.src.rpm
 # ovirt depends on ovirt-engine-yarn
 sudo mv /var/lib/mock/fedora-32-x86_64/result/*.noarch.rpm /var/repo/noarch/
 
